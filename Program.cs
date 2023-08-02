f0cabc9b7e20cdaf6cfc49a900ff1588840bb0bc
IvaSlavcheva <141230716+IvaSlavcheva@users.noreply.github.com>
2023-08-02 15:18:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string brand = Console.ReadLine();
+            double initialPrice = double.Parse(Console.ReadLine());
+            double days = double.Parse(Console.ReadLine());
+
+            double taxes = (20.0 / 100.0) * initialPrice;
+            double priceAfterTaxes = initialPrice + taxes + 275;
+            double resellValue = priceAfterTaxes +(days*20);
+          
+            double profit = (15.0 / 100.0) * resellValue;
+            Console.WriteLine($"The {brand} with initial price of {initialPrice} BGN will sell for {resellValue} BGN ");
+            Console.WriteLine($"Profit {profit} BGN");
+        }
+    }
+}
